7fc990add88d8a9415b0cf2063d294b2cf1766d8
Danilo Josino <danilo@josino.com.br>
2013-11-19 14:38:47 -0300

    require gems

--- lib/acts_as_boleto.rb
+++ lib/acts_as_boleto.rb
@@ -1,9 +1,9 @@
 #require 'rubygems'
 #require 'barby'
-#require 'barby/barcode/code_25_interleaved'
-#require 'barby/outputter/png_outputter'
 #require 'prawn'
-#require 'prawn/measurement_extensions'
+require 'barby/barcode/code_25_interleaved'
+require 'barby/outputter/png_outputter'
+require 'prawn/measurement_extensions'
 path = File.dirname(__FILE__) + "/acts_as_boleto/"
 ["acts_as_boleto", "acts_as_boleto_helper"].each do |library|
    require path + library
